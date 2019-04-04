2c5badd25200f46eaccf40ce0968b5cc4902753f
szincone <sawyerzincone@gmail.com>
2019-04-04 12:06:49 -0600

    added substring ternary to make boxes somewhat consistent

--- src/containers/AllBeers.js
+++ src/containers/AllBeers.js
@@ -5,15 +5,42 @@ export default function AllBeers(props) {
   return (
     <>
       <Link to="/add_new_beer">
-        <button value="addNew">Add New</button>
+        <button
+          value="addNew"
+          style={{
+            background: 'red',
+            fontWeight: 'bold',
+            border: 'none',
+            borderRadius: '4px',
+            cursor: 'pointer',
+            padding: '.25rem .5rem',
+          }}
+        >
+          Add New
+        </button>
       </Link>
       <div style={{ display: 'flex', flexWrap: 'wrap' }}>
         {props.beers
           .filter((beer) => beer.name !== '')
           .map((beer) => (
-            <Link to={`/beers/${beer.id}`} key={beer.id}>
-              <div style={{ background: 'red', margin: '1rem' }}>
-                <h1>{beer.name}</h1>
+            <Link
+              to={`/beers/${beer.id}`}
+              style={{ textDecoration: 'none', color: 'white' }}
+              key={beer.id}
+            >
+              <div
+                style={{
+                  background: 'red',
+                  margin: '1rem',
+                  textAlign: 'center',
+                  padding: '1rem',
+                }}
+              >
+                <h1>
+                  {beer.name.length > 10
+                    ? beer.name.substring(0, 10) + '...'
+                    : beer.name}
+                </h1>
                 <h1>{beer.likes}</h1>
               </div>
             </Link>
